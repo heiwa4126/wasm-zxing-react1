03a9d01740b08d40e1613c3c8aa09f1aece510c3
heiwa4126 <heiwa4126@gmail.com>
2024-09-11 16:51:39 +0900

    [WIP] update

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,62 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import {
+	ReadResult,
+	type ReaderOptions,
+	readBarcodesFromImageData,
+} from "zxing-wasm/reader";
 import "./App.css";
 import Qr5 from "./assets/qr5.jpg";
+
+const readerOptions: ReaderOptions = {
+	tryHarder: true,
+	formats: ["QRCode"],
+	maxNumberOfSymbols: 16,
+};
 
 function App() {
 	const imgRef = useRef<HTMLImageElement>(null);
 	const canvasRef = useRef<HTMLCanvasElement>(null);
+	const [qrResult, setQrResult] = useState<ReadResult[]>();
 
-	// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
 	useEffect(() => {
-		if (!imgRef.current || !canvasRef.current) {
-			return undefined;
-		}
-
 		const img = imgRef.current;
 		const canvas = canvasRef.current;
 
+		if (!img || !canvas) {
+			return;
+		}
+
 		const handleImageLoad = () => {
 			const ctx = canvas.getContext("2d");
+			if (!ctx) {
+				// 古いブラウザで getContext("2d") が null を返す可能性がある。ほぼ無い
+				console.error("Canvas 2D context is not supported.");
+				return;
+			}
 			canvas.width = img.naturalWidth;
 			canvas.height = img.naturalHeight;
-			ctx?.drawImage(img, 0, 0);
+			ctx.drawImage(img, 0, 0);
+			const imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+			readBarcodesFromImageData(imgData, readerOptions)
+				.then((result) => {
+					setQrResult(result);
+					console.log(result);
+				})
+				.catch((error) => {
+					console.error(error);
+				});
 		};
 
 		if (img.complete) {
 			handleImageLoad();
-			return undefined;
+			return;
 		}
 
 		img.addEventListener("load", handleImageLoad);
 		return () => {
 			img.removeEventListener("load", handleImageLoad);
 		};
-	}, [imgRef.current, canvasRef.current]);
+	}, []);
 
 	return (
 		<>
@@ -46,6 +71,15 @@ function App() {
 				alt="QRCode Example No.5 (3024 x 1935)"
 				style={{ visibility: "hidden", width: 0, height: 0 }}
 			/>
+			{qrResult && (
+				<ul>
+					{qrResult.map((result, i) => (
+						<li key={`${i}${result.text}`}>
+							Format: {result.format}, Text: {result.text}
+						</li>
+					))}
+				</ul>
+			)}
 		</>
 	);
 }
