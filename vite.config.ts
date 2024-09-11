4c077ec16360265cdfdddef7cc991b9fe181f85d
heiwa4126 <heiwa4126@gmail.com>
2024-09-11 17:52:48 +0900

    [WIP] update some

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	base: process.env.GITHUB_REPO_NAME ?? "./",
 	build: {
 		minify: "esbuild",
 		rollupOptions: {
