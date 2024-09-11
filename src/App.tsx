9a0df4ef46e34a872a5926184fc2a850970477b0
heiwa4126 <heiwa4126@gmail.com>
2024-09-11 16:19:59 +0900

    [WIP] backup

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,50 @@
+import { useEffect, useRef } from "react";
 import "./App.css";
 import Qr5 from "./assets/qr5.jpg";
 
 function App() {
+	const imgRef = useRef<HTMLImageElement>(null);
+	const canvasRef = useRef<HTMLCanvasElement>(null);
+
+	// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+	useEffect(() => {
+		if (!imgRef.current || !canvasRef.current) {
+			return undefined;
+		}
+
+		const img = imgRef.current;
+		const canvas = canvasRef.current;
+
+		const handleImageLoad = () => {
+			const ctx = canvas.getContext("2d");
+			canvas.width = img.naturalWidth;
+			canvas.height = img.naturalHeight;
+			ctx?.drawImage(img, 0, 0);
+		};
+
+		if (img.complete) {
+			handleImageLoad();
+			return undefined;
+		}
+
+		img.addEventListener("load", handleImageLoad);
+		return () => {
+			img.removeEventListener("load", handleImageLoad);
+		};
+	}, [imgRef.current, canvasRef.current]);
+
 	return (
 		<>
 			<h1>wasm-zxing-react1</h1>
+			<canvas
+				ref={canvasRef}
+				style={{ display: "block", width: "800px", height: "auto" }}
+			/>
 			<img
+				ref={imgRef}
 				src={Qr5}
 				alt="QRCode Example No.5 (3024 x 1935)"
-				style={{ display: "block", width: "800px", height: "auto" }}
+				style={{ visibility: "hidden", width: 0, height: 0 }}
 			/>
 		</>
 	);
